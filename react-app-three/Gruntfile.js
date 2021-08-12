8ee07253d6dcc5e8f8e845f0786291c83247f134
Mahesh Sampekatte <mseenapp@mathworks.com>
2021-08-12 17:44:56 -0400

    making grunt work

--- react-app-three/Gruntfile.js
+++ react-app-three/Gruntfile.js
@@ -2,21 +2,45 @@ const TerserPlugin = require('terser-webpack-plugin');
 const webpackConfig = require('./webpack.config.js');
 
 module.exports = (grunt) => {
-    grunt.initConfig({
-      webpack: {
-        // eslint-disable-next-line
-        prod: Object.assign({
-          stats: process.env.NODE_ENV === 'production',
-          optimization: {
-            minimizer: [new TerserPlugin({
-              extractComments: true,
-            })],
-          },
-        }, webpackConfig),
+  grunt.initConfig({
+    webpack: {
+      // eslint-disable-next-line
+      prod: Object.assign({
+        stats: process.env.NODE_ENV === 'production',
+        optimization: {
+          minimizer: [new TerserPlugin({
+            extractComments: true,
+          })],
+        },
+      }, webpackConfig),
+    },
+    connect: {
+      server: {
+        options: {
+          keepalive: true,
+          port: 8100,
+          protocol: 'http',
+          hostname: '*',
+          base: '.',
+          middleware: function (connect, options, middlewares) {
+            middlewares.unshift(function (req, res, next) {
+              res.setHeader('Access-Control-Allow-Origin', '*');
+              res.setHeader('Access-Control-Allow-Methods', "POST, GET");
+              res.setHeader('Access-Control-Allow-Headers', "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+              next();
+            });
+            return middlewares;
+          }
+        }
       }
-    });
-  
-  
-    // Compile (e.g minification)
-    grunt.registerTask('compile', ['webpack:prod']);
+    }
+  });
+
+
+
+  // Compile (e.g minification)
+  grunt.registerTask("default", ["connect:server"]);
+  grunt.registerTask('compile', ['webpack:prod']);
+  grunt.loadNpmTasks('grunt-webpack');
+  grunt.loadNpmTasks('grunt-contrib-connect');
 };  
